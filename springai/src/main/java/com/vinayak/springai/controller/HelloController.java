4a3c9c9b694dd58cdf417c997e3f8c453890f461
Vinayak Upadhyay <vinayak7317783517@gmail.com>
2025-11-09 22:08:44 +0530

    Implemented the api which will use Some famous Personality name and will you acheivement usinf API with PromptTemplate

--- springai/src/main/java/com/vinayak/springai/controller/HelloController.java
+++ springai/src/main/java/com/vinayak/springai/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.vinayak.springai.controller;
 
+import java.util.Map;
+
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,4 +30,27 @@ public class HelloController {
 				.getOutput()
 				.getText();
 	}
+	
+	@GetMapping("/celeb")
+	public String getCelebDetails(@RequestParam String name) {
+		String message = "List the details of Famous personality {name} along with "
+				+ "their Carrier achievments. Show the details in readable format.";
+		
+		PromptTemplate template = new PromptTemplate(message);
+		
+		Prompt prompt = template.create(
+				Map.of("name" , name)
+		);
+		
+		return chatClient
+				.prompt(prompt)
+				.call()
+				.chatResponse()
+				.getResult()
+				.getOutput()
+				.getText();
+		
+	}
+	
+	
 }
