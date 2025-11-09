11dc1a4c3fd9f01bedbcd5fff747c6fc9d868f78
Vinayak Upadhyay <vinayak7317783517@gmail.com>
2025-11-09 22:27:51 +0530

    Integrated API with Spring Templatte of Prompt so in Bussiness Logic will be more cleaner and code readablity will be maintained

--- springai/src/main/java/com/vinayak/springai/controller/HelloController.java
+++ springai/src/main/java/com/vinayak/springai/controller/HelloController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	
 	private final ChatClient chatClient;
+	
+	@Value("classpath:/prompts/celeb-details.st")
+	private Resource celebPrompt;
 	
 	public HelloController(ChatClient.Builder builder) {
 		this.chatClient = builder.build();
@@ -33,10 +38,11 @@ public class HelloController {
 	
 	@GetMapping("/celeb")
 	public String getCelebDetails(@RequestParam String name) {
-		String message = "List the details of Famous personality {name} along with "
-				+ "their Carrier achievments. Show the details in readable format.";
+		//String message = "List the details of Famous personality {name} along with "
+		//		+ "their Carrier achievements. Show the details in readable format.";
 		
-		PromptTemplate template = new PromptTemplate(message);
+		//PromptTemplate template = new PromptTemplate(message);
+		PromptTemplate template = new PromptTemplate(celebPrompt);
 		
 		Prompt prompt = template.create(
 				Map.of("name" , name)
